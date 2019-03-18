a6c9b27e6875d49c104e79b5010ccf50d8f021d2
satindergrewal <grewal@lavabit.com>
2019-03-19 04:30:08 +1300

    comments update

--- rectangle/main2.c
+++ rectangle/main2.c
@@ -3,14 +3,17 @@
 
 int main(int argc, char *argv[]) {
     
-    double width = atoi(argv[1]);
+    // Declaring variables
+    double width = atoi(argv[1]); // converting char argument values to integer/float using atoi function
     double height = atoi(argv[2]);
     double area = 0.0;
     double perimeter = 0.0;
 
+    // Calculating perimeter and area of rectangle
     perimeter = 2.0 * (height + width);
     area = height * width;
 
+    // Displaying the calculated numbers
     printf("Rectangle Height \t: %0.2f\n", height);
     printf("Rectangle Width \t: %0.2f\n", width);
     printf("Rectangle Area \t\t: %0.2f\n", area);
