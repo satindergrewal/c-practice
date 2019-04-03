7d3d6e92fee094844f6cb7140d40275247f8a920
satindergrewal <grewal@lavabit.com>
2019-04-03 17:08:45 +1300

    char count v2

---
+++ TCPL/chapter1/char_count2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* count characters in input; 2nd version */
+
+int main()
+{
+    double nc;
+
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
+
+    return 0;
+}
+
+/*
+$ ./char_count2 
+abcd
+efgh
+10
+*/
