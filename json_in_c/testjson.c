85b9611833548e9a9c2ded5ffdcac1d84f26e73b
satindergrewal <grewal@lavabit.com>
2021-01-11 00:18:27 +1300

    updated to test nanomsg json

--- json_in_c/testjson.c
+++ json_in_c/testjson.c
@@ -8,11 +8,12 @@ int main() {
 	cJSON *root;
 	cJSON *info;
 
-	root = cJSON_CreateObject();
-
-	cJSON_AddItemToObject(root, "userInfo", info = cJSON_CreateObject());
-	cJSON_AddStringToObject(info, "userEmail", "satinder");
-	cJSON_AddStringToObject(info, "userPassword", "verystrongpassword");
+	// root = cJSON_CreateObject();
+	// cJSON_AddItemToObject(root, "userInfo", info = cJSON_CreateObject());
+	
+	info = cJSON_CreateObject();
+	cJSON_AddStringToObject(info, "method", "live");
+	cJSON_AddStringToObject(info, "id", "19e6443f0fb3a7d30d9ff7e696320d509eb263270f36d8a968ea407012fc438a");
 	char *info_to_string = NULL;
 	info_to_string =  cJSON_Print(info);
 	printf("%s\n", info_to_string);
@@ -25,4 +26,9 @@ int main() {
 // {
 // 	"userEmail":	"satinder",
 // 	"userPassword":	"verystrongpassword"
+// }
+
+// {
+//     "method":    "live",
+//     "id":    "19e6443f0fb3a7d30d9ff7e696320d509eb263270f36d8a968ea407012fc438a"
 // }
