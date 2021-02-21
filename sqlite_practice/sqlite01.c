a678b4841a0df5a81413d06569b21a74ceca0427
satindergrewal <grewal@lavabit.com>
2021-02-21 22:14:53 +1300

    concat strings, println, getenv to join sqlite path with HOME dir

--- sqlite_practice/sqlite01.c
+++ sqlite_practice/sqlite01.c
@@ -1,12 +1,56 @@
 #include <stdio.h>
 #include <sqlite3.h> 
+#include <stdlib.h>       // calloc
+#include <stdarg.h>       // va_*
+#include <string.h>       // strlen, strcpy
+
+void println(char *line)
+{
+    printf("%s\n", line);
+}
+
+char* concat(int count, ...)
+{
+    va_list ap;
+    int i;
+
+    // Find required length to store merged string
+    int len = 1; // room for NULL
+    va_start(ap, count);
+    for(i=0 ; i<count ; i++)
+        len += strlen(va_arg(ap, char*));
+    va_end(ap);
+
+    // Allocate memory to concat strings
+    char *merged = calloc(sizeof(char),len);
+    int null_pos = 0;
+
+    // Actually concatenate strings
+    va_start(ap, count);
+    for(i=0 ; i<count ; i++)
+    {
+        char *s = va_arg(ap, char*);
+        strcpy(merged+null_pos, s);
+        null_pos += strlen(s);
+    }
+    va_end(ap);
+
+    return merged;
+}
+
 
 int main(int argc, char* argv[]) {
+   char *db_path;
+
+   db_path = concat(2,getenv("HOME"),"/.chipsln/chips/lightningd.sqlite3");
+   println(db_path);
+   free(db_path);
+
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
 
-   rc = sqlite3_open("/Users/satinder/.chipsln/chips/lightningd.sqlite3", &db);
+   rc = sqlite3_open(db_path, &db);
 
    if( rc ) {
       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
